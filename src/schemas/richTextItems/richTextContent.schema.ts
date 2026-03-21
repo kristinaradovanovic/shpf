031ab083f3b465465728fdcd00604c5879ebad5b
Kristina Radovanovic <cristinaradovanovic@gmail.com>
2026-03-21 11:55:57 +0100

    fix: remove video from rich text

--- src/schemas/richTextItems/richTextContent.schema.ts
+++ src/schemas/richTextItems/richTextContent.schema.ts
@@ -1,4 +1,4 @@
-import { LinkIcon, PlayIcon } from '@sanity/icons';
+import { LinkIcon } from '@sanity/icons';
 import { defineArrayMember, defineField, defineType } from 'sanity';
 
 export default defineType({
@@ -121,106 +121,6 @@ export default defineType({
         // ],
       },
     },
-    defineArrayMember({
-      name: 'video',
-      title: 'video',
-      type: 'object',
-      fields: [
-        defineField({
-          name: 'videoReference',
-          title: 'Video',
-          type: 'object',
-          fields: [
-            defineField({
-              name: 'title',
-              title: 'Title',
-              type: 'string',
-              validation: (rule) => rule.required(),
-            }),
-            defineField({
-              name: 'description',
-              title: 'Description',
-              type: 'text',
-            }),
-            defineField({
-              name: 'videoType',
-              title: 'Video Type',
-              type: 'string',
-              options: {
-                list: [
-                  { title: 'Uploaded', value: 'video' },
-                  { title: 'Embedded', value: 'embeddedVideoUrl' },
-                ],
-                layout: 'radio',
-              },
-              initialValue: 'video',
-              validation: (rule) => rule.required(),
-            }),
-            defineField({
-              name: 'video',
-              title: 'Video File',
-              type: 'file',
-              options: {
-                accept: 'video/*',
-              },
-              hidden: ({ parent }) => parent?.videoType !== 'video',
-            }),
-            defineField({
-              name: 'embeddedVideoUrl',
-              title: 'Embedded Video URL',
-              type: 'url',
-              hidden: ({ parent }) => parent?.videoType !== 'embeddedVideoUrl',
-            }),
-          ],
-        }),
-        defineField({
-          name: 'showDescription',
-          title: 'Show Description?',
-          description:
-            'Turn the toggle on to display the descriptive text below the video on the website',
-          type: 'boolean',
-          initialValue: false,
-        }),
-        defineField({
-          name: 'shouldAutoplay',
-          title: 'Should the video autoplay?',
-          description:
-            'Turn the toggle on to autoplay the video when the page loads. If autoplay is on, the video will be muted.',
-          type: 'boolean',
-          initialValue: false,
-        }),
-        defineField({
-          name: 'onMute',
-          title: 'Should the video be muted?',
-          description:
-            'Turn the toggle on to have the video muted. This will only work if autoplay is turned off.',
-          type: 'boolean',
-          initialValue: false,
-          hidden: ({ parent }) => parent?.shouldAutoplay === true,
-        }),
-      ],
-      preview: {
-        select: {
-          title: 'videoReference.title',
-          videoType: 'videoReference.videoType',
-          shouldAutoplay: 'shouldAutoplay',
-          onMute: 'onMute',
-        },
-        prepare({ title, videoType, shouldAutoplay, onMute }) {
-          const videoTypeTitle = videoType === 'video' ? 'Uploaded Video' : 'Embedded Video';
-          const autoplayTitle = shouldAutoplay ? ' | With autoplay ' : ' | No autoplay ';
-          const onMuteTitle = onMute || shouldAutoplay ? ' | Muted ' : ' | Not muted ';
-
-          const subtitle = videoTypeTitle + autoplayTitle + onMuteTitle;
-
-          return {
-            title: title || 'Untitled Video',
-            subtitle: subtitle,
-            icon: PlayIcon,
-          };
-        },
-      },
-    }),
     defineArrayMember({
       type: 'image',
       title: 'Image',
